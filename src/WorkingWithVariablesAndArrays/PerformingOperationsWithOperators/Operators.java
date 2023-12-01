402174a0b7f0866ed549b7cf57031597bacf8232
Jordan Nitta-Rodrigues <jnittar@wgu.edu>
2023-12-01 12:56:07 -0800

    Notes

--- src/WorkingWithVariablesAndArrays/PerformingOperationsWithOperators/Operators.java
+++ src/WorkingWithVariablesAndArrays/PerformingOperationsWithOperators/Operators.java
@@ -2,6 +2,36 @@ package WorkingWithVariablesAndArrays.PerformingOperationsWithOperators;
 
 public class Operators {
     public static void main(String[] args){
+//        Arithmetic operators
+    double x = 2;
+    int y = 3;
+    double z = x + y;
+    System.out.println(z);
+    double d = x / y;
+    System.out.println(d);
+//    Doesn't change the value of the variable
+    System.out.println(x - y);
+    int nr1 = 10;
+    int nr2 = 5;
+    int result = nr1 % nr2;
+    System.out.println(result);
 
+//    Assignment Operator
+    int nr3 = 7;
+    int nr4 = 70;
+//        Increasing and assigning
+    nr3 += nr4;
+    System.out.print(nr3);
+
+//     Unary Operator
+    int nr5 = 8;
+
+    //increase the value by one
+    nr5++;
+    System.out.println(nr5);
+
+    int nr6 = 3;
+    System.out.println(-nr6);
+    System.out.println(nr6);
     }
 }
---
+++ src/WorkingWithVariablesAndArrays/PerformingOperationsWithOperators/operators.md
@@ -0,0 +1,25 @@
+# Operators
+
+1. Arithmethic Operators
+```java
+    +, %, -, *, /
+```
+   
+2. Assignment Operators
+```java
+=
++=
+-=
+*=
+/=
+%=
+```
+3. Unary Operators<br>
+   1.Only one operand
+```js
+    -x
+    !x
+x++ and ++x : increment by one
+x-- and --x : decrement by one
+```
+4. Relational Operators
