1e363bcc145bd9140dd510bc02b9729350f58c0f
Jordan Nitta-Rodrigues <jnittar@wgu.edu>
2023-12-01 11:44:30 -0800

    Learning unit 2

---
+++ src/WorkingWithVariablesAndArrays/UnderstandingVariables/Variables.java
@@ -0,0 +1,4 @@
+package WorkingWithVariablesAndArrays.UnderstandingVariables;
+
+public class Variables {
+}
---
+++ src/WorkingWithVariablesAndArrays/WorkingWithPrimitives/Primitives.java
@@ -0,0 +1,24 @@
+package WorkingWithVariablesAndArrays.WorkingWithPrimitives;
+
+public class Primitives {
+    public static void main(String[] args){
+        byte b = 34;
+
+        short s = 23000;
+
+        int i = 45000;
+//        Tell java the number on screen is a long need a L at the end
+        long l = 12345678901L;
+
+//        Need to specify with an F at the end
+        float f = 3.2f;
+        double d = 3.23232232323;
+
+        char c = 'c';
+        char c1 = 45;
+
+        boolean playMe = true;
+        boolean comeOutAndPlay = false;
+
+    }
+}
